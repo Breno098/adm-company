22f14a62dad5983a95a1912aee3de5fd9fad6e7b
Breno098 <brenohenrique098@gmail.com>
2021-09-09 09:00:17 -0300

    Child Component

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Child from './Child'
 import { HasError, AlertError, AlertSuccess } from 'vform'
 
 import Modal from './Modal'
 
 // Components that are registered globaly.
 [
+  Child,
   HasError,
   AlertError,
   AlertSuccess,
