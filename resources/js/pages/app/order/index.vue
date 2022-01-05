f90502c8881fad1b48bc1413d341fbaaf7a2dced
Breno098 <brenohenrique098@gmail.com>
2022-01-04 23:07:53 -0300

    Index Orders

--- resources/js/pages/app/order/index.vue
+++ resources/js/pages/app/order/index.vue
@@ -11,7 +11,7 @@
           height="4"
           bottom
           absolute
-          :active="loading"
+          :active="table.loading"
         ></v-progress-linear>
 
         <v-spacer></v-spacer>
@@ -27,27 +27,141 @@
       </v-toolbar>
     </v-card>
 
+    <v-expansion-panels class="mb-4">
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <span>
+            <v-icon :size="15">mdi-magnify</v-icon>
+            Filtros
+          </span>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-row>
+             <v-col cols="12">
+              <v-select
+                v-model="table.filters.statuses_ids"
+                vali
+                :items="statuses"
+                item-text="name"
+                item-value="id"
+                label="STATUS"
+                outlined
+                dense
+                :loading="table.loading"
+                multiple
+                no-data-text="Nenhum status encontrado"
+              ></v-select>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <v-text-field
+                label="CLIENTE"
+                outlined
+                dense
+                v-model="table.filters.client_name"
+                :loading="table.loading"
+                @input="table.filters.client_name = table.filters.client_name.toUpperCase()"
+                v-on:keyup.enter="_load"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <v-text-field
+                label="ENDEREÇO"
+                outlined
+                dense
+                v-model="table.filters.address"
+                :loading="table.loading"
+                @input="table.filters.address = table.filters.address.toUpperCase()"
+                v-on:keyup.enter="_load"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-card-actions class="pb-4">
+            <v-spacer></v-spacer>
+            <v-btn
+              color="btnPrimary"
+              @click="_load"
+              class="px-5"
+              rounded
+            >
+              Buscar <v-icon dark class="ml-2">mdi-magnify</v-icon>
+            </v-btn>
+            <v-btn
+              color="btnCleanFilter"
+              @click="_eraser"
+              class="px-5"
+              rounded
+            >
+              Limpar <v-icon dark class="ml-2">mdi-eraser</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
     <v-row>
-      <v-col cols="12" md="6" v-for="status in statuses" :key="status.name">
-        <router-link :to="{ name: 'order.list', params: { statusId: status.id } }" style="text-decoration: none">
-          <v-hover v-slot="{ hover }">
-              <v-card :class="{ 'on-hover': hover }" :elevation="hover ? 12 : 2">
-                <v-card-text class="d-flex flex-row justify-space-between align-center py-10 px-8">
-                  <h2> {{ status.name }} </h2>
-                  <v-badge
-                    :content="status.orders ? status.orders.length : 0"
-                    :value="status.orders ? status.orders.length : 0"
-                    color="primary"
-                    overlap
-                  >
-                    <v-icon size="50" :color="status.color">{{ status.icon }}</v-icon>
-                  </v-badge>
-                </v-card-text>
-              </v-card>
-          </v-hover>
-        </router-link>
+      <v-col cols="12" v-if="table.loading">
+        <v-row>
+          <v-col cols="12" md="6" v-for="index in [0, 1, 2, 3]" :key="index">
+            <v-skeleton-loader type="image"></v-skeleton-loader>
+          </v-col>
+        </v-row>
       </v-col>
-    </v-row>
+
+      <v-col cols="12" v-for="order in table.items" :key="order.id" v-else>
+        <v-hover v-slot="{ hover }">
+          <v-card
+            v-on:click="_edit(order.id)"
+            :class="{ 'on-hover': hover }"
+            :elevation="hover ? 12 : 2"
+          >
+            <v-list-item three-line>
+              <v-list-item-content v-if="order.client">
+                <div class="text-overline mb-4 d-flex justify-space-between">
+                  {{ order.client.name }}
+                  <v-chip
+                    v-if="order.status"
+                    class="py-2"
+                    label
+                    :color="order.status.color"
+                    outlined
+                  >
+                    {{ order.status.name }}
+                    <v-icon :color="order.status.color" class="ml-2">{{ order.status.icon }}</v-icon>
+                  </v-chip>
+                </div>
+                <v-list-item-subtitle v-if="order.address">
+                  {{ order.address.street }} {{ order.address.number ? `n° ${order.address.number}` : '' }}, {{ order.address.district }} - {{ order.address.city }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-spacer/>
+              <v-btn text color="primary" v-on:click="_edit(order.id)" small>
+                Ver informações
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
+      </v-col>
+
+      <v-col cols="12">
+        <div class="text-center pt-2 mt-4">
+          <v-pagination
+            v-show="table.pageCount > 1"
+            v-model="table.page"
+            :length="table.pageCount"
+            @input="_load"
+            :total-visible="15"
+            color="primary"
+          ></v-pagination>
+        </div>
+      </v-col>
+  </v-row>
 </div>
 </template>
 
@@ -60,50 +174,94 @@ export default {
     return { title: 'Ordens de Serviços' }
   },
   data: () => ({
-    loading: false,
+    table: {
+      filters: {
+        status_id: null,
+        client_name: '',
+        address: '',
+        statuses_ids: []
+      },
+      orderBy: 'created_at:desc',
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      items: [],
+      loading: false,
+    },
     statuses: []
   }),
+  computed: {
+    statusIdByRoute(){
+      return this.$route.params.statusId ? this.$route.params.statusId : '';
+    }
+  },
   mounted() {
     this._start();
   },
   methods: {
     async _start(){
-      await this._loadStatuses();
+      this._load();
+
+      this._loadStatuses();
+    },
+    async _load(){
+      let params = {
+        page: this.table.page,
+        itemsPerPage: this.table.itemsPerPage,
+        orderBy: this.table.orderBy,
+        relations: ['client','address', 'status'],
+        ...this.table.filters
+      }
+
+      this.table.loading = true;
+      await axios.get(`/api/order`, { params }).then(response => {
+        console.log(response.data);
+        if(response.data.data.data.length === 0 && this.table.page != 1){
+          this.table.page = 1;
+          this._load()
+        }
+
+        this.table.loading = false;
+
+        if(response.data.success){
+          this.table.items = response.data.data.data;
+          this.table.pageCount = response.data.data.last_page;
+        } else {
+          this.$refs.fireDialog.error({ title: 'Erro ao carregar pedidos'});
+        }
+      });
+    },
+    _edit(id){
+      this.$router.push({ name: 'order.show', params: { id } })
+    },
+     _add(){
+      this.$router.push({ name: 'order.create' })
+    },
+    _eraser(){
+      for (const field in this.table.filters) {
+        this.table.filters[field] = null;
+      }
+
+      this._load();
     },
     async _loadStatuses(){
-      this.loading = true;
+      this.table.loading = true;
 
       let params = {
         type: 'order',
         orderBy: 'order',
-        relations: [ 'orders' ]
       }
 
       await axios.get(`/api/status`, { params }).then(response => {
         if(response.data.success){
-          this.statuses = response.data.data;
-
-          let orders_all = [];
-
-          this.statuses.forEach(status => status.orders.length > 0 ? orders_all.push(status.orders) : null );
-
-          this.statuses.push({
-            name: 'TODOS',
-            icon: 'mdi-format-list-bulleted',
-            orders: orders_all
-          });
-
-          return;
+          return this.statuses = response.data.data;
         }
 
         this.$refs.fireDialog.error({ title: 'Error ao carregar pedidos' })
       });
 
-      this.loading = false;
+      this.table.loading = false;
     },
-     _add(){
-      this.$router.push({ name: 'order.create' })
-    }
   }
 }
 </script>
